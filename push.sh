7eea609a80c89adbf16d0de3a28c430f44be5495
zhangjinhui <871506263@qq.com>
2021-06-28 19:14:38 +0800

    first commit

---
+++ push.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+#输入./push命令提交代码，只需要info变量中配置提交信息
+current=`date "+%Y-%m-%d %H:%M:%S"`
+info="提交代码于: "$current
+#切到当前绝对路径
+cd `pwd`
+git add -A
+git commit -m "$info"
+git pull
+git push
+echo '已经成功push'
