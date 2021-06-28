6627143f4bb274430c8060e0c8547d36ddbaf588
steve zhang <871506263@qq.com>
2021-06-29 00:30:38 +0800

    提交代码于: 2021-06-29 00:30:38

--- demo-001.ts
+++ demo-001.ts
@@ -1,3 +1,31 @@
-let num: number = 1
-let firstname: string = 'jinhui'
-let lastname: string = String('zhang')
+// 1.0 字符串
+// let fullname: string = 'zhang, jinhui';
+// let age: string = String(123);
+// let myinfo = `my nam is ${fullname}, i am ${age} years old`
+// console.log('fullname :>> ', fullname);
+// console.log('age :>> ', age);
+// console.log('myinfo :>> ', myinfo);
+
+// 2.0 数字
+/** 十进制整数 */
+let integer: number = 6;
+
+/** 十进制整数 */
+let integer2: number = Number(42);
+
+/** 十进制浮点数 */
+let decimal: number = 3.14;
+console.log('decimal :>> ', decimal);
+
+/** 二进制整数 */
+let binary: number = 0b1010; // 0b开头
+console.log('binary :>> ', binary);
+
+/** 八进制整数 */
+let octal: number = 0o744; // 0o开头
+
+/** 十六进制整数 */
+let hex: number = 0xf00d; // 0x开头
+
+// let big: bigint =  100n;
+// console.log('big :>> ', big);
