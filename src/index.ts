3bee36eb20b23feef7b28b13ddb3ab3e4e3e811c
steve zhang <871506263@qq.com>
2021-11-13 12:18:18 +0800

    提交代码于: 2021-11-13 12:18:17

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,1 @@
-let arrayNum: number[] = [1, 2, 3];
-console.log('arrayNum :>> ', arrayNum);
-let arrayNum1: Array<number> = [4, 5, 6];
+require('./lesson/03.ts');
---
+++ src/lesson/03.ts
@@ -0,0 +1,3 @@
+let arrayNum: number[] = [1, 2, 3];
+console.log('arrayNum :>> ', arrayNum);
+let arrayNum1: Array<number> = [4, 5, 6];
