1f622b96713a0f26f8fd85e90af4e40ae5b42cc6
steve zhang <871506263@qq.com>
2022-08-18 23:43:30 +0800

    提交代码于: 2022-08-18 23:43:30

--- src/lesson/03.ts
+++ src/lesson/03.ts
@@ -1,3 +1,34 @@
-let arrayNum: number[] = [1, 2, 3];
-console.log('arrayNum :>> ', arrayNum);
-let arrayNum1: Array<number> = [4, 5, 6];
+// 1.数组
+// let arrayNum: number[] = [1, 2, 3];
+// console.log('arrayNum :>> ', arrayNum);
+// let arrayNum1: Array<string | number> = [4, 'a', 6];
+// console.log('arrayNum1', arrayNum1)
+
+// type arrType = number | string;
+// let arrayOfNumber: arrType[] = [1, 2, 3]; // 提示 ts(2322)
+// arrayOfNumber[3] = '4'; // 提示 ts(2322)
+// console.log('arrayOfNumber', arrayOfNumber);
+// arrayOfNumber.push('b'); // 提示 ts(2345)
+// console.log('arrayOfNumber', arrayOfNumber);
+
+//2.元组
+// let tom: [string, number] = ['', 0];
+// tom = ['Tom', 25];
+// tom.push('t');
+// tom.push(2);
+// // tom.push(true);
+// console.log('tom', tom);
+
+// any
+// let anything: any = {};
+// let z = anything.x.y.z; // z 类型是 any，不会提示错误
+// const res = z(); // 不会提示错误
+
+// unknown
+let result: unknown;
+// result = 1;
+// result = 'h';
+// let num: number = result; // 提示 ts(2322)
+// let str: string = result; // 提示 ts(2322)
+let ang: any = result; // 不会提示错误
+let unn: unknown = result; // 不会提示错误
